ffe9a0debd41c210ea19394af8235627842be17a
unknown <umrbekmasharopov23@gmail.com>
2022-10-04 16:11:44 +0500

    shop page

--- src/Assets/pages/Shop.jsx
+++ src/Assets/pages/Shop.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from 'react'
+import ForTitle from '../../Components/For-title'
+import Gamecatalog from '../../Components/Main/Gamecatalog'
 
 const Shop = () => {
     return (
         <div>
-             <h1>shop</h1>
+            <div className='shop_wrapper'>
+                <div className='container'>
+                    <ForTitle
+                        title='Horror'
+                    />
+                    <Gamecatalog
+                        img='./img/spider-men.jpg'
+                        game='Spider Man Remastred'
+                        version='for windows 10'
+                        price='10 $'
+                    />
+                </div>
+            </div>
         </div>
-    );
+    )
 }
 
-export default Shop;
+export default Shop
---
+++ src/Components/For-title.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ForTitle = (props) => {
+    return (
+        <div>
+            <h3 className='main_title'>
+                {props.title}
+            </h3>
+        </div>
+    );
+}
+
+export default ForTitle;
